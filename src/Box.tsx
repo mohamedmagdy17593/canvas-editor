37e3846b72fa7ede8fda1d8fc6ca0665a8cf1293
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2022-10-29 19:21:46 +0200

    use-moveable

--- src/Box.tsx
+++ src/Box.tsx
@@ -1,22 +1,89 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Rnd } from 'react-rnd';
 import { useAppState } from './App';
+import Moveable from 'react-moveable';
 
 function Box() {
-  let appState = useAppState();
+  let [target, setTarget] = React.useState<any>();
+  let [elementGuidelines, setElementGuidelines] = React.useState<any>([]);
+  let [frame, setFrame] = React.useState({
+    translate: [0, 0],
+  });
+
+  console.log('elementGuidelines', elementGuidelines);
+  useEffect(() => {
+    setElementGuidelines(
+      Array.from(
+        document
+          .querySelector('.EditorArea__canvas')
+          ?.querySelectorAll('.Box') ?? [],
+      ).filter((el) => el !== target),
+    );
+  }, [target]);
+
   return (
-    <Rnd
-      className="Box Drag-Box"
-      default={{
-        x: 0,
-        y: 0,
-        width: 200,
-        height: 200,
-      }}
-      dragGrid={[10, 10]}
-      resizeGrid={[10, 10]}
-      scale={appState.scale}
-    ></Rnd>
+    <>
+      <div
+        className="Box"
+        ref={(e) => {
+          setTarget(e);
+        }}
+      >
+        Target
+      </div>
+      <Moveable
+        target={target}
+        padding={{ left: 0, top: 0, right: 0, bottom: 0 }}
+        keepRatio={false}
+        renderDirections={['nw', 'n', 'ne', 'w', 'e', 'sw', 's', 'se']}
+        zoom={1}
+        edge={false}
+        origin={false}
+        // resize
+        resizable
+        throttleResize={1}
+        onResizeStart={(e) => {
+          e.setOrigin(['%', '%']);
+          e.dragStart && e.dragStart.set(frame.translate);
+        }}
+        onResize={(e) => {
+          const beforeTranslate = e.drag.beforeTranslate;
+          frame.translate = beforeTranslate;
+          e.target.style.width = `${e.width}px`;
+          e.target.style.height = `${e.height}px`;
+          e.target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`;
+        }}
+        // drag
+        draggable
+        throttleDrag={0}
+        onDragStart={(e) => {
+          e.set(frame.translate);
+        }}
+        onDrag={(e) => {
+          frame.translate = e.beforeTranslate;
+        }}
+        onRender={(e) => {
+          const { translate } = frame;
+          e.target.style.transform = `translate(${translate[0]}px, ${translate[1]}px)`;
+        }}
+        // snap
+        snappable={true}
+        elementGuidelines={elementGuidelines}
+        verticalGuidelines={[0, 200, 400]}
+        horizontalGuidelines={[0, 200, 400]}
+        snapThreshold={5}
+        isDisplaySnapDigit={true}
+        snapGap={true}
+        snapDirections={{ top: true, right: true, bottom: true, left: true }}
+        elementSnapDirections={{
+          top: true,
+          right: true,
+          bottom: true,
+          left: true,
+        }}
+        snapDigit={0}
+      />
+    </>
   );
 }
 
--- src/EditorArea.tsx
+++ src/EditorArea.tsx
@@ -1,11 +1,5 @@
 import React, { useEffect } from 'react';
-
-import { createUseGesture, dragAction, pinchAction } from '@use-gesture/react';
-import { useSpring, animated } from '@react-spring/web';
 import Box from './Box';
-import { appState } from './App';
-
-const useGesture = createUseGesture([dragAction, pinchAction]);
 
 function EditorArea() {
   useEffect(() => {
@@ -20,66 +14,18 @@ function EditorArea() {
     };
   }, []);
 
-  let [style, api] = useSpring(() => ({
-    x: 0,
-    y: 0,
-    scale: 1,
-    rotateZ: 0,
-  }));
-
   let editorRef = React.useRef<HTMLDivElement>(null);
   let ref = React.useRef<HTMLDivElement>(null);
 
-  useGesture(
-    {
-      onDrag: ({ pinching, cancel, offset: [x, y], event, ...rest }) => {
-        if (pinching) return cancel();
-
-        if (
-          [editorRef.current, ref.current].includes(
-            event.target as HTMLDivElement,
-          )
-        ) {
-          api.start({ x, y });
-        }
-      },
-      onPinch: ({
-        origin: [ox, oy],
-        first,
-        movement: [ms],
-        offset: [s, a],
-        memo,
-      }) => {
-        if (first) {
-          const { width, height, x, y } = ref.current!.getBoundingClientRect();
-          const tx = ox - (x + width / 2);
-          const ty = oy - (y + height / 2);
-          memo = [style.x.get(), style.y.get(), tx, ty];
-        }
-
-        const x = memo[0] - (ms - 1) * memo[2];
-        const y = memo[1] - (ms - 1) * memo[3];
-        api.start({ scale: s, rotateZ: a, x, y });
-        appState.scale = s;
-        return memo;
-      },
-    },
-    {
-      target: editorRef,
-      drag: { from: () => [style.x.get(), style.y.get()] },
-      pinch: { scaleBounds: { min: 0.5, max: 2 }, rubberband: false },
-    },
-  );
-
   return (
-    <animated.div ref={editorRef} className="EditorArea">
-      <animated.div ref={ref} style={style} className="EditorArea__canvas">
+    <div ref={editorRef} className="EditorArea">
+      <div ref={ref} className="EditorArea__canvas">
         <Box />
         <Box />
         <Box />
         <Box />
-      </animated.div>
-    </animated.div>
+      </div>
+    </div>
   );
 }
 
